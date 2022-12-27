0f054bd15193ece4b0c72a9cc1cdd2acf9d97939
davidinmicahel <davidinmichael@gmail.com>
2022-12-27 22:06:35 +0100

    I added repeat features to the fruit bowl game

--- c-programming_projects/fruit-bowl.c
+++ c-programming_projects/fruit-bowl.c
@@ -32,9 +32,43 @@ int main()
     printf("Would you want to play again?\nEnter 1 for Yes and 2 for No.\n");
     scanf("%d", &playAgain);
 
+    if(playAgain == 2)
+    {
+        printf("Thank you for playing, hope you had a great time.");
+    }
+    else if(playAgain == 1)
+    {
+        do
+        {
+        //Prompt user for inputs
+        printf("Enter Number of Fruits: ");
+        scanf("%d", &userInput);
+
+        //Display the outputs
+        printf("Amount of Apple: %d\n", userInput / 2);
+        printf("Amount of Banana: %d\n", userInput / 2);
+        printf("Amount of Pie you can make: %d\n", (userInput / 2) / 3);
+
+        printf("Would you want to play again?\nEnter 1 for Yes and 2 for No.\n");
+        scanf("%d", &playAgain);
+        }while(playAgain == 1);
+    }
+    else
+    {
+        printf("\n%d is not a valid option, please enter a valid option\n", playAgain);
+        while(playAgain > 2)
+        {
+            //Prompt user for inputs
+            printf("\nEnter 1 for Yes and 2 for No.\n");
+            scanf("%d", &playAgain);
+
+        }
+        
+    }
+
     do
     {
-    //Prompt user for inputs
+        //Prompt user for inputs
     printf("Enter Number of Fruits: ");
     scanf("%d", &userInput);
 
@@ -45,9 +79,43 @@ int main()
 
     printf("Would you want to play again?\nEnter 1 for Yes and 2 for No.\n");
     scanf("%d", &playAgain);
-    }while(playAgain == 1);
 
+    if(playAgain == 2)
+    {
+        printf("Thank you for playing, hope you had a great time.");
+    }
+    else if(playAgain == 1)
+    {
+        do
+        {
+        //Prompt user for inputs
+        printf("Enter Number of Fruits: ");
+        scanf("%d", &userInput);
+
+        //Display the outputs
+        printf("Amount of Apple: %d\n", userInput / 2);
+        printf("Amount of Banana: %d\n", userInput / 2);
+        printf("Amount of Pie you can make: %d\n", (userInput / 2) / 3);
+
+        printf("Would you want to play again?\nEnter 1 for Yes and 2 for No.\n");
+        scanf("%d", &playAgain);
+        }while(playAgain == 1);
+    }
+    else
+    {
+        //printf("\n%d is not a valid option, please enter a valid option\n", playAgain);
+        while(playAgain > 2)
+        {
+            //Prompt user for inputs
+            printf("\n%d is not a valid option, please enter a valid option, Enter 1 for Yes and 2 for No.\n", playAgain);
+            scanf("%d", &playAgain);
+
+        }
+        
+    }
+    } while(playAgain == 1);
     
+
 
     printf("\nIf you found this program helpful, please leave a like.\n\nAlso, if you have any questions, contributions or corrections, please comment it.\nThank you.\n");
     
