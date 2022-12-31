ec134ae1298d7f0e54a210ea20c16f9365f704f1
davidinmicahel <davidinmichael@gmail.com>
2022-12-31 23:38:31 +0100

    I added comments to the digital clock file

--- c-programming_projects/digitalClock.c
+++ c-programming_projects/digitalClock.c
@@ -1,58 +1,54 @@
-// This is a program for displaying a digital clock.
-//Set your time by inputing the values.
+/**
+ * Write a program that prints a given row of pyramid
+*/
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
+int add(int a, int b)
+{
+    int result = a + b;
+    //return result;
+    printf("%d", result);
+}
+
+int sub(int a, int b)
+{
+    int result = a - b;
+    //return result;
+    printf("%d", result);
+}
 
 int main()
 {
-    int h;
-    int m;
-    int i;
+    int fN, sN;
+    char sign;
 
-    printf("Set The Time\n");
-    printf("Enter Hour: ");
-    scanf("%d", &h);
+    printf("Choose Operator: ");
+    scanf("%c", &sign);
 
-    printf("Enter Minute: ");
-    scanf("%d", &m);
+    printf("Enter Number1: ");
+    scanf("%d", &fN);
 
-    if(h > 12 || h < 0 || m > 60 || m < 0)
+    printf("Enter Number2: ");
+    scanf("%d", &sN);
+
+    if(sign == '+')
     {
-        printf("\nInvalid 12 Hours Time Format\n");
-        exit(0);
+        //int result = add(fN, sN);
+        //return result;
+        //printf("%d", result);
+        add(fN, sN);
     }
-
-
-    for(int i = 1; i <= 60; i++)
+    else if(sign == '-')
     {
-        while(i <= 60)
-        {
-            printf("Time: ");
-            i++;
-            printf("%02dh : %02dm : %02ds ", h, m, i);
-            sleep(1);
-            system("cls");
-
-            if(i == 60)
-            {
-                m++;
-                i = 0;
-            }
-            if(m == 60)
-            {
-                h++;
-                m = 0;
-            }
-            if(h > 12)
-            {
-                h = 1;
-            }
-            
-        }
+        int result1 = sub(fN, sN);
+        return result1;
     }
+    else
+    {
+        printf("Invalid operator");
+    }
+    
     
 
     return 0;
