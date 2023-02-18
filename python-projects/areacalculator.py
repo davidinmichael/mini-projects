afcd3b5b4ddae29ce46e024fff73a4be66b789df
davidinmichael <davidinmichael@gmail.com>
2023-02-18 23:05:26 +0100

    I added the area calculator file

---
+++ python-projects/areacalculator.py
@@ -0,0 +1,9 @@
+name = input("What is your name: ")
+print(f"Hello {name}!")
+print(f"Welcome to the Rectangle Area Calculator\nSo, let's calculate the area of your rectangle")
+height = int(input("Enter the height(m): "))
+width = int(input("Enter the width(m): "))
+
+area = height * width
+print(f"The area is: {area}m^2")
+print("If you found this program useful, please leave a star/like\nIf you have any contributions, questions or suggestions, please leave it in the comment.\nThank you\n")
