fd97da12779a84fc65e17488b77ae25ecf57a74b
davidinmichael <davidinmichael@gmail.com>
2023-08-18 10:44:29 +0100

    I modified the program to print all elements in one line

--- dart-projects/fizzbuzz.dart
+++ dart-projects/fizzbuzz.dart
@@ -1,19 +1,16 @@
+import 'dart:io';
+
 void main() {
   var startNum;
-  // var endNum = 50;
-  for (startNum = 1; startNum <= 50; startNum++){
-    if (startNum % 3 == 0 && startNum % 5 == 0){
-      print("FizzBuzz");
-    }
-    else if (startNum % 3 == 0){
-      print('fizz ');
-    }
-    else if (startNum % 5 == 0){
-      print("buzz ");
-    }
-    else{
-      print(startNum);
+  for (startNum = 1; startNum <= 50; startNum++) {
+    if (startNum % 3 == 0 && startNum % 5 == 0) {
+      stdout.write("FizzBuzz ");
+    } else if (startNum % 3 == 0) {
+      stdout.write('fizz ');
+    } else if (startNum % 5 == 0) {
+      stdout.write("buzz ");
+    } else {
+      stdout.write("$startNum ");
     }
   }
-
 }
