15d077ee10309bdd779e3bfb93c6e4d34724612c
davidinmichael <davidinmichael@gmail.com>
2023-08-18 10:57:34 +0100

    It can now take input to start and stop

--- dart-projects/fizzbuzz.dart
+++ dart-projects/fizzbuzz.dart
@@ -1,16 +1,20 @@
 import 'dart:io';
 
 void main() {
-  var startNum;
-  for (startNum = 1; startNum <= 50; startNum++) {
-    if (startNum % 3 == 0 && startNum % 5 == 0) {
+  stdout.write("Enter the start number: ");
+  var startNum = int.parse(stdin.readLineSync()!);
+  stdout.write("Enter the end number: ");
+  var endNum = int.parse(stdin.readLineSync()!);
+  
+  for (var start = startNum; start <= endNum; start++) {
+    if (start % 3 == 0 && start % 5 == 0) {
       stdout.write("FizzBuzz ");
-    } else if (startNum % 3 == 0) {
+    } else if (start % 3 == 0) {
       stdout.write('fizz ');
-    } else if (startNum % 5 == 0) {
+    } else if (start % 5 == 0) {
       stdout.write("buzz ");
     } else {
-      stdout.write("$startNum ");
+      stdout.write("$start ");
     }
   }
 }
